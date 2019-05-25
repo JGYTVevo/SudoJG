500c494e99a3f940ece6eb9b9539166e2af0fd52
JGYTVevo <36227813+JGYTVevo@users.noreply.github.com>
2019-05-25 13:38:45 +1200

    add src plugin.yml main.php

---
+++ SudoJG/src/JGYTVevo/SudoJG/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace JGYTVevo\SudoJG;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as c;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+        $this->getLogger()->notice(c::GREEN." Sudo has been enabled.");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        if(strtolower($command->getName()) == "sudo"){
+            if($sender instanceof Player){
+                if($sender->hasPermission("sudo.command")){
+                    if(count($args) === 0 || count($args) === 1){
+                        $sender->sendMessage("Usage: /sudo <player> <message or /command..>");
+                    }else{
+                        $player = $this->getServer()->getPlayer($args[0]);
+                        if($player !== null){
+                            $player->chat(implode(" ", array_slice($args, 1, 1000)));
+                        }else{
+                            $sender->sendMessage(c::RED.$args[0]." is not online");
+                        }
+                    }
+                }
+            }else{
+                $this->getLogger()->warning(c::RED." You can not use this command in console!");
+            }
+        }
+        return true;
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->warning(c::RED." Sudo has been disabled.");
+    }
+}
